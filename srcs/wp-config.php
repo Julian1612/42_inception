77ae2caac61d00d63fb74e61489b8bd55f5d27f0
Julian <92728359+Julian1612@users.noreply.github.com>
2023-11-06 13:27:33 +0100

    added log.txt

---
+++ srcs/wp-config.php
@@ -0,0 +1,9 @@
+<?php
+define('DB_NAME', 'wordpress');
+define('DB_USER', 'wordpress');
+define('DB_PASSWORD', 'your_db_password');
+define('DB_HOST', 'db');
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
+
+// ... other WordPress configurations ...
