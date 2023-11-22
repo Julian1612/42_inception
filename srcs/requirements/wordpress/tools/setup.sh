cedcad2c6f55f41884a784f0c47a16ebcc6cdda1
Julian <92728359+Julian1612@users.noreply.github.com>
2023-11-22 09:44:16 +0100

    safe

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,29 +1,20 @@
 #!/bin/bash
 
-# change directory to /var/www/wordpress
 cd /var/www/wordpress
-
-# download wp
 wp core download --allow-root
-
-# create wp-config.php file and configure the wp instance to accept the MariaDB databank
 wp core config	\
-				--dbname=$WP_DB_NAME \
-				--dbuser=$WP_DB_USER \
-				--dbpass=$WP_DB_PASSWORD \
-				--dbhost=$WP_DB_HOST \
+				--dbname=$DB_NAME \
+				--dbuser=$DB_USER \
+				--dbpass=$DB_PASSWORD \
+				--dbhost=$DB_HOST \
 				--allow-root
-
-# install the wp instance and set the admin user and general infos like title and url
 wp core install \
-				--admin_user=$WP_ADMIN_USER \
+				--admin_user=$WP_ADMIN_NAME \
 				--admin_password=$WP_ADMIN_PASSWORD \
 				--admin_email=$WP_ADMIN_MAIL \
 				--title=$WP_TITLE \
 				--url=$WP_URL \
 				--allow-root
-
-# create the first non-admin user
 wp user create \
 				$WP_USER \
 				$WP_USER_MAIL \
@@ -31,8 +22,5 @@ wp user create \
 				--user_pass=$WP_USER_PASSWORD \
 				--allow-root
 
-# cd back to where you were before
 cd -
-
-# Start php-fpm to respond to FastCGI requests and force it to stay in the foreground
 php-fpm7.3 -F
--- to-do.md
+++ to-do.md
@@ -4,11 +4,13 @@
 - make the project ready for evaluation
 - prepare for exam (one level every day)
 ################## TO-DO-TODAY ###################
+@todo make the project run on the virtual machine
+@todo check with eval sheet
 @todo make the 2nd task of the examen
 
 ##################### INBOX ######################
-@todo change name of env variables
-@todo check with eval sheet
+@todo checkout if its possible to add in the setup.sql env variables
+and then change the content (username password etc.) to env variables
 @todo check setup.conf form nginx (is similar to alex)
 	- why do i need to do this ?
 	- what is it for ?
